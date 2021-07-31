59f34b16bd0e7237df45ac6a2c0b348bd0d0f4f8
derGoldeneJonas <jonas@bender-home.eu>
2021-07-31 02:00:25 +0200

    Updates

---
+++ precache-manifest.b5c9d0076f002cc9e35d228751d1a9f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef6c2194291d4e65d9a9bfa4ee73b3d",
+    "url": "/webshop/index.html"
+  },
+  {
+    "revision": "27d5f34080fffcf1294e",
+    "url": "/webshop/static/css/main.c87bcac7.chunk.css"
+  },
+  {
+    "revision": "2c5a81bc50a065dde21a",
+    "url": "/webshop/static/js/2.8d49a798.chunk.js"
+  },
+  {
+    "revision": "fbf7a8d3bbea906e2d23f11649844255",
+    "url": "/webshop/static/js/2.8d49a798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d5f34080fffcf1294e",
+    "url": "/webshop/static/js/main.f50ec3bc.chunk.js"
+  },
+  {
+    "revision": "1bb454b0e2ac8290d4c8",
+    "url": "/webshop/static/js/runtime-main.7360cb70.js"
+  },
+  {
+    "revision": "ca1453fe3cec70570c0c2f42602aa855",
+    "url": "/webshop/static/media/Ernstings_family.ca1453fe.png"
+  },
+  {
+    "revision": "28a304acd288123d6da098094bde1811",
+    "url": "/webshop/static/media/icon.28a304ac.svg"
+  }
+]);
